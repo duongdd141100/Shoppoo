995c6c8c97316408ccc2e6dd8e3e8ebaf1b92c71
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-07-17 15:43:16 +0700

    Handle register button

--- app/src/main/java/com/example/shoppoo/RegisterActivity.java
+++ app/src/main/java/com/example/shoppoo/RegisterActivity.java
@@ -1,13 +1,20 @@
 package com.example.shoppoo;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.example.shoppoo.common.Constant;
+import com.example.shoppoo.entity.Shop;
+import com.example.shoppoo.entity.User;
 import com.example.shoppoo.service.RegisterService;
 import com.example.shoppoo.service.impl.RegisterServiceImpl;
 
@@ -18,6 +25,24 @@ public class RegisterActivity extends AppCompatActivity {
     private RelativeLayout shopView;
 
     private Button btnRegister;
+
+    private User user;
+
+    private Shop shop;
+
+    private ActivityResultLauncher<Intent> startActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == 1) {
+                        Intent intent = result.getData();
+                        user = (User) intent.getSerializableExtra("user");
+                        shop = (Shop) intent.getSerializableExtra("shop");
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +55,10 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister = findViewById(R.id.btn_register);
         shopView.setVisibility(View.GONE);
 
-        RegisterService registerService = new RegisterServiceImpl(this, userView, shopView, btnRegister);
+        RegisterService registerService = new RegisterServiceImpl(this, userView, shopView, btnRegister, user, shop, startActivity);
         registerService.setCategorySpinnerValues();
+        registerService.setInfoValues();
         registerService.handleShopChecked();
+        registerService.handleRegisterButton();
     }
 }
--- app/src/main/java/com/example/shoppoo/service/RegisterService.java
+++ app/src/main/java/com/example/shoppoo/service/RegisterService.java
@@ -1,7 +1,13 @@
 package com.example.shoppoo.service;
 
 public interface RegisterService {
+
     void setCategorySpinnerValues();
+
     void handleShopChecked();
+
     void handleRegisterButton();
+
+    void setInfoValues();
+
 }
